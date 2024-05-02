34193a684edb06dad2463a6e0a687960dd5e9e6f
nk-on <2nkoz@proton.me>
2024-05-02 21:46:40 +0400

    Feat:adding sqaures using javascript

--- src/boardCreation.js
+++ src/boardCreation.js
@@ -1,3 +1,9 @@
-const boardCreation = document.querySelector('[data-board]');
+const gameBoard = document.querySelector('[data-board]');
 function createBoard() {
-}
+  for (let i = 0; i < 16; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    gameBoard.appendChild(square);
+  }
+};
+createBoard();
