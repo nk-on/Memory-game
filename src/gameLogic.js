0e45d8460ec24a340bbbfa88ed22b52fa847e185
nk-on <2nkoz@proton.me>
2024-05-03 13:42:50 +0400

    Feat:shuffling the fruits array

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,1 +1,59 @@
-import gameBoard from "./boardCreation.js";
+import gameBoard from './boardCreation.js';
+//create 16 objects with according titles and links to images
+//shuffle randomly this fruits array
+const fruits = [
+  {
+    title: 'Apple',
+    img: '/assets/apple.png',
+  },
+  {
+    title: 'Banana',
+    img: '/assets/banana.png',
+  },
+  {
+    title: 'Watermelon',
+    img: '/assets/watermelon.png',
+  },
+  {
+    title: 'Strawberry',
+    img: '/assets/strawberry.png',
+  },
+  {
+    title: 'Apple',
+    img: '/assets/apple.png',
+  },
+  {
+    title: 'Banana',
+    img: '/assets/banana.png',
+  },
+  {
+    title: 'Watermelon',
+    img: '/assets/watermelon.png',
+  },
+  {
+    title: 'Strawberry',
+    img: '/assets/strawberry.png',
+  },
+  {
+    title: 'Apple',
+    img: '/assets/apple.png',
+  },
+  {
+    title: 'Banana',
+    img: '/assets/banana.png',
+  },
+  {
+    title: 'Watermelon',
+    img: '/assets/watermelon.png',
+  },
+  {
+    title: 'Strawberry',
+    img: '/assets/strawberry.png',
+  },
+];
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
