9d808c233f8f1a1e35052164b5bf56bda36ceadb
nk-on <2nkoz@proton.me>
2024-05-04 13:50:03 +0400

    Fix:fixed the bug - in particular fruits array hadn't same lenght as squares which caused user's inability to rotate cards

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -52,6 +52,22 @@ const fruits = [
     title: 'Strawberry',
     img: '/assets/strawberry.png',
   },
+  {
+    title: 'Apple',
+    img: '/assets/apple.png',
+  },
+  {
+    title: 'Banana',
+    img: '/assets/banana.png',
+  },
+  {
+    title: 'Watermelon',
+    img: '/assets/watermelon.png',
+  },
+  {
+    title: 'Strawberry',
+    img: '/assets/strawberry.png',
+  },
 ];
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
