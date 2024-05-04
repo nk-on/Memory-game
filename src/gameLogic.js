e80c0547bff96ec2fc9666dea70871780c089b0e
nk-on <2nkoz@proton.me>
2024-05-04 13:44:01 +0400

    Feat:Implementing logic of checking matching squares

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,7 +1,8 @@
 import gameBoard from './boardCreation.js';
 //create 16 objects with according titles and links to images
 //shuffle randomly this fruits array
-let rotatedSquares = 0;
+let [rotatedSquares, guessedSquares] = [0, 0];
+let selectedFruits = [];
 const fruits = [
   {
     title: 'Apple',
@@ -60,20 +61,34 @@ function shuffleArray(array) {
 }
 shuffleArray(fruits);
 const squares = Array.from(gameBoard.children);
-function compareImages(square, index) {
-  
+function compareSquares() {
+  //do titles match ? if so leave them as they are
+  //if not let user to continue rotating hide with question mark again
+  const fruit1 = selectedFruits[0];
+  const fruit2 = selectedFruits[1];
+  console.log(fruit1, fruit2);
+  if (fruit1.title === fruit2.title) {
+    guessedSquares++;
+  } else {
+    squares.forEach((square) => {
+      square.style.backgroundImage = 'url(assets/questionMark.png)';
+    });
+  }
 }
 function rotateSquare(square, index) {
-  if (rotatedSquares >= 2) {
-    compareImages(square, index);
+  if (rotatedSquares > 1) {
+    compareSquares();
+    selectedFruits = [];
     rotatedSquares = 0;
     return;
-  };
+  }
+  selectedFruits.push(fruits[index]);
   square.style.backgroundImage = `url(${fruits[index].img})`;
   rotatedSquares++;
 }
 squares.forEach((square, index) => {
   square.addEventListener('click', () => {
+    console.log(index)
     rotateSquare(square, index);
   });
 });
